113551cb5ad5ee925d4165398d9084b09ca2df5d
aarticontractor <contaaru@gmail.com>
2023-05-07 21:02:45 -0700

    created POST & GET API routes for blogpost model

---
+++ controllers/api/blogpostRoutes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const {
+  Blogpost
+} = require('../../models');
+
+router.post('/new', async (req, res) => {
+  const { user_id, title, content } = req.body;
+  const blogpost = await Blogpost.create({
+    user_id,
+    title,
+    content,
+    date_created: new Date(),
+  });
+  res.status(201).json(blogpost);
+});
+
+
+
+router.get('/all', async (req, res) => {
+  const blogposts = await Blogpost.findAll();
+  res.json(blogposts);
+});
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
