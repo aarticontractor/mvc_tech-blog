e519babc3946d5e8d9a83e2405d618704cf83f5e
aarticontractor <contaaru@gmail.com>
2023-05-07 18:54:26 -0700

    Added user login API and made some checks

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const bcrypt = require('bcrypt');
+
 
 router.post('/signup', async (req, res) => {
   try {
@@ -22,6 +24,34 @@ router.post('/signup', async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Failed to create user.' });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if username and password are provided
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required.' });
+  }
+
+  try {
+    // Check if user exists in the database
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password.' });
+    }
+
+    // Compare the entered password with the hashed password in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid username or password.' });
+    }
+
+    res.status(200).json({ message: 'Login successful.' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to login.' });
   }
 });
 
