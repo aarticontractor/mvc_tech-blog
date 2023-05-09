8586924346bfa292f9f23436025848dbe3a28c57
aarticontractor <contaaru@gmail.com>
2023-05-08 19:33:40 -0700

    updated dashboard.js to add new comments

--- public/dashboard.js
+++ public/dashboard.js
@@ -23,3 +23,34 @@ document.getElementById('new-blogpost-form').addEventListener('submit', async (e
         alert('Failed to create the blog post.');
     }
 });
+
+
+const commentForms = document.querySelectorAll('.comment-form');
+
+commentForms.forEach((form) => {
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const comment = form.comment.value;
+    const blogpostId = form.blogpost_id.value;
+
+    const response = await fetch('/api/comment/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user_id: sessionStorage.getItem('user_id'),
+        blogpost_id: blogpostId,
+        content: comment,
+      }),
+    });
+
+    if (response.ok) {
+      alert('Comment added successfully!');
+      location.reload();
+    } else {
+      alert('Failed to add the comment.');
+    }
+  });
+});
